939f6ffc7b33836a7f1f48302b667c934c8a6382
Mihail Konoplitskyi <107000632+mihail-konoplitskyi@users.noreply.github.com>
2022-09-26 12:19:28 +0300

    Create Example.swift
    Added example.swift file to detect repo language

---
+++ Example.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initViewController()
+    }
+    
+    private func initViewController() {
+    }
+}
